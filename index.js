9e2928cacfdbe03f9035f1e14ec543e46107a219
Mateus Santos <mateus.sanjo20@gmail.com>
2024-10-09 10:26:00 -0300

    Criando outra função, agora para excluir a contagem das quebras de linhas e contar por paragráfos

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const caminho = process.argv;
 const link = caminho[2];
 
 fs.readFile(link, "utf-8", (erro, texto) => {
-  verificarPalavrasDuplicadas(texto);
+  contagemPorParagrafos(texto);
 });
+
+function contagemPorParagrafos(texto) {
+  const paragrafos = texto.toLowerCase().split("\n");
+  const contagem = paragrafos.map((paragrafo) => {
+    return verificarPalavrasDuplicadas(paragrafo);
+  });
+  console.log(contagem);
+}
 
 // Função para capturar cada palavra e a quantidade que tem nela em cada parte do texto
 function verificarPalavrasDuplicadas(texto) {
@@ -15,5 +23,5 @@ function verificarPalavrasDuplicadas(texto) {
   listaPalavras.forEach((palavra) => {
     resultado[palavra] = (resultado[palavra] || 0) + 1;
   });
-  console.log(resultado);
+  return resultado;
 }
