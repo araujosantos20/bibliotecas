1506a332e5baafece35e02f73c92db522e8467a1
Mateus Santos <mateus.sanjo20@gmail.com>
2024-10-09 10:19:00 -0300

    Criando uma função para a contagem de palavras repetidas em arquivos de texto

--- index.js
+++ index.js
@@ -4,5 +4,16 @@ const caminho = process.argv;
 const link = caminho[2];
 
 fs.readFile(link, "utf-8", (erro, texto) => {
-  console.log(texto);
+  verificarPalavrasDuplicadas(texto);
 });
+
+// Função para capturar cada palavra e a quantidade que tem nela em cada parte do texto
+function verificarPalavrasDuplicadas(texto) {
+  const listaPalavras = texto.split(" ");
+  const resultado = {};
+  // Agora vamos iterar os objetos que vão aparecer
+  listaPalavras.forEach((palavra) => {
+    resultado[palavra] = (resultado[palavra] || 0) + 1;
+  });
+  console.log(resultado);
+}
